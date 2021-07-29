3c2f7868839bcef79ae344b6bfa58c1aa070b87f
6e3veR6k <bezyn4uk@gmail.com>
2021-07-29 23:35:07 +0300

    Finished

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,26 @@
 'use strict';
 
-const str = `All works ${5 % 2}`;
+const uploadButton = document.querySelector('.button');
+const buttonText = document.querySelector('.btn-text');
 
-console.log(str);
+const newText = `<i class='bx bx-loader-circle'></i>
+      <span class="btn-text">Uploading...</span>`;
+
+const defaultText = `<i class="bx bx-cloud-upload"></i>
+      <span class="btn-text">Upload File</span>`;
+
+uploadButton.addEventListener('click', e => {
+    const button = e.currentTarget;
+    button.preventDefault;
+    button.classList.add('progress');
+    button.textContent = '';
+    button.insertAdjacentHTML('afterbegin', newText);
+
+    setTimeout(() => {
+        button.classList.remove('progress');
+        button.textContent = '';
+        button.insertAdjacentHTML('afterbegin', defaultText);
+    }, 6000);
+});
+
+console.log({uploadButton, buttonText});
